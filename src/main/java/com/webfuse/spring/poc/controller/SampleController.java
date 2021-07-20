0080c1164dd929bbfceaa2e4b0997c052de28879
Webfuse <webfuse2017>
2021-07-20 22:47:51 +0530

    Changes related to Swagger UI v3

--- src/main/java/com/webfuse/spring/poc/controller/SampleController.java
+++ src/main/java/com/webfuse/spring/poc/controller/SampleController.java
@@ -3,7 +3,7 @@ package com.webfuse.spring.poc.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,7 +18,7 @@ public class SampleController {
 	}
 
 	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+	public String handleFileUpload(@RequestPart("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 
 		System.out.println("You successfully uploaded " + file.getOriginalFilename() + "!");
 
